13d7d646bf2660b522ea8d5a80b7e0d8fcdd0e96
Saloni <subedisaloni2002@gmail.com>
2021-07-07 07:50:28 +0530

    ⚡ states and props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
-import Menu from "./Components/MenuComponent"
+import Menu from "./Components/MenuComponent";
+import {DISHES} from "./shared/dishes";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu/>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes : DISHES
+    };
+  }
+  
+  render(){
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu dishes={this.state.dishes} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
